e2c5bfe86814cb08135265647edb80eff98e50ad
ethan <ethanmannette@gmail.com>
2015-07-13 11:47:07 -0600

    Added link to javascript file in HTML. Added function with if/else statement.

---
+++ main.js
@@ -0,0 +1,13 @@
+// Function return Vs. Console.log
+
+function name() {
+
+  if (name.length % 2 === 0) {
+    console.log ("Well " + name + ", you have an even amount of letters in your name!");
+  };
+  else {
+    console.log ("Well " + name + ", you have an odd amount of letters in your name!");
+  };
+
+};
+name('John')
